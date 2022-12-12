c570de5a131dfdd1f4f52e26057a66780b55a1a6
Daniel Bril <brildan@post.bgu.ac.il>
2022-12-12 17:19:29 +0200

    sfsdf

--- src/main/java/bguspl/set/ex/Table.java
+++ src/main/java/bguspl/set/ex/Table.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
-
+//fwfwffw
 /**
  * This class contains the data that is visible to the player.
  *
